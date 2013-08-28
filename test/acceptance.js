a1d6813e9947ed03d134b9a26164680b0cc486c0
Matteo Figus <matteo@swapit.co.uk>
2013-08-28 23:53:54 +0100

    Added a test utils class to help handling the servers manipulations

--- test/acceptance.js
+++ test/acceptance.js
@@ -1,67 +1,37 @@
 var apiBenchmark = require('./../index');
-var express = require('express');
 var should = require('should');
 var superagent = require('superagent');
-
-var services = {};
-
-var endpoints = { simpleRoute: '/getJson', secondaryRoute: '/getJson2' };
-
-var setupServer = function(name, serverPort, delay, callback){
-  services[name] = "http://localhost:" + serverPort;
-  
-  var app = express();
-  
-  app.get(endpoints.simpleRoute, function(req, res){
-    setTimeout(function(){
-      res.json({ message: "ok" });
-    }, delay);
-  });   
-
-  app.get(endpoints.secondaryRoute, function(req, res){
-    setTimeout(function(){
-      res.json({ message: "secondaryRoute" });
-    }, delay);
-  }); 
-
-  app.listen(serverPort, callback);
-};
-
-var setupServers = function(callback){
-  var servers = 2;
-
-  var done = function(){
-    servers --;
-    if(servers == 0)
-      callback();
-  };
-
-  setupServer("Slow server", 3006, 200, done);
-  setupServer("Fast server", 3007, 0, done);
-};
+var TestServers = require('./utils/test-servers');
 
 describe('compare function', function(){
 
+  var testServers,
+      endpoints = { simpleRoute: '/getJson', secondaryRoute: '/getJson2' };
+
   before(function(done){
-    setupServers(done);
+
+    var servers = [{ name: "Slow server", port: 3006, delay: 200 }, 
+                   { name: "Fast server", port: 3007, delay: 0}];
+
+    testServers = new TestServers(endpoints, servers, done);
   });
 
   it('should correctly recognize the fastest service', function(done) {
-    apiBenchmark.compare(services, { simpleRoute: endpoints.simpleRoute }, { maxTime: 0.5 }, function(results){
+    apiBenchmark.compare(testServers.services, { simpleRoute: endpoints.simpleRoute }, { maxTime: 0.5 }, function(results){
       results['Fast server'].isFastest.should.be.eql(true);
       done();
     });
   });
 
   it('should work without the optional options parameter', function(done) {
-    apiBenchmark.compare(services, { simpleRoute: endpoints.simpleRoute }, function(results){
+    apiBenchmark.compare(testServers.services, { simpleRoute: endpoints.simpleRoute }, function(results){
       results['Fast server'].isFastest.should.be.eql(true);
       done();
     });
   });
 
   it('should correctly compare multiple routes', function(done) {
-    apiBenchmark.compare(services, endpoints, { debug: true }, function(results){
+    apiBenchmark.compare(testServers.services, endpoints, { debug: true }, function(results){
       results['Fast server'].isFastest.should.be.eql(true);
       done();
     });
---
+++ test/utils/test-servers.js
@@ -0,0 +1,37 @@
+var express = require('express');
+
+module.exports = function(endpoints, servers, callback){
+  this.services = {};
+  this.endpoints = endpoints;
+  this.app = {};
+
+  this.setupRoute = function(route, response, delay){
+    this.app.get(route, function(req, res){
+      setTimeout(function(){
+        res.json(response);
+      }, delay);
+    });   
+    return this;
+  };
+
+  this.setupServer = function(name, serverPort, delay, callback){
+
+    this.services[name] = "http://localhost:" + serverPort;
+    this.app = express();
+
+    for(routeName in this.endpoints)
+      this.setupRoute(this.endpoints[routeName], { message: routeName }, delay);
+
+    this.app.listen(serverPort, callback);
+  };
+
+  var serversInitialized = 0;
+  var done = function(){
+    serversInitialized ++;
+    if(serversInitialized == servers.length)
+      callback();
+  };
+
+  for(var i = 0; i < servers.length; i++)
+    this.setupServer(servers[i].name, servers[i].port, servers[i].delay, done); 
+};
