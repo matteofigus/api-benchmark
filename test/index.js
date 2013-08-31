750f42523e3dab0b4a270de0c3895640f78a265b
Matteo Figus <matteo@swapit.co.uk>
2013-08-31 19:18:08 +0100

    Running the unit tests first is better

--- test/index.js
+++ test/index.js
@@ -1,33 +1,30 @@
 var Mocha = require('mocha'),
-    fs = require('fs'),
-    path = require('path');
+    fs = require('fs');
 
-var acceptance = new Mocha({
-	timeout: 20000
-});
+var runAcceptanceTests = function(callback){
+	var acceptance = new Mocha({timeout: 20000 });
+	console.log("Running acceptance tests...");
 
-console.log("Running acceptance tests...");
-acceptance.addFile('./test/acceptance.js');
-acceptance.run(function(failures){
+	acceptance.addFile('./test/acceptance.js');
+	acceptance.run(callback);
+};
 
-	if(failures)
-		process.exit(failures);
-
+var runUnitTests = function(callback){
 	var unit = new Mocha();
-
 	console.log("Running unit tests...");
 
-	fs.readdirSync('./test/unit/').filter(function(file){
-	    return file.substr(-3) === '.js';
+	fs.readdirSync('./test/unit/')
+		.filter(function(file){ return file.substr(-3) === '.js';})
+		.forEach(function(file){ unit.addFile('./test/unit/' + file); });
 
-	}).forEach(function(file){
-	    unit.addFile(
-	        path.join('./test/unit/', file)
-	    );
-	});
+	unit.run(callback);
+};
 
-
-	unit.run(function(failures){
-	  process.exit(failures);
-	});
+runUnitTests(function(failures){
+	if(failures)
+		process.exit(failures);
+	else
+		runAcceptanceTests(function(failures){
+			process.exit(failures);
+		});
 });
