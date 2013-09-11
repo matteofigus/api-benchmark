922313496e2e2392d551136ceb54397d3e97e183
Matteo Figus <matteo@swapit.co.uk>
2013-09-11 20:14:01 +0100

    Cleaning on tests executor

--- test/index.js
+++ test/index.js
@@ -2,16 +2,16 @@ var Mocha = require('mocha'),
     fs = require('fs');
 
 var runAcceptanceTests = function(callback){
-	var acceptance = new Mocha({timeout: 20000 });
 	console.log("Running acceptance tests...");
+	var acceptance = new Mocha({timeout: 20000 });
 
 	acceptance.addFile('./test/acceptance.js');
 	acceptance.run(callback);
 };
 
 var runUnitTests = function(callback){
-	var unit = new Mocha();
 	console.log("Running unit tests...");
+	var unit = new Mocha();
 
 	fs.readdirSync('./test/unit/')
 		.filter(function(file){ return file.substr(-3) === '.js';})
@@ -24,7 +24,5 @@ runUnitTests(function(failures){
 	if(failures)
 		process.exit(failures);
 	else
-		runAcceptanceTests(function(failures){
-			process.exit(failures);
-		});
+		runAcceptanceTests(process.exit);
 });
